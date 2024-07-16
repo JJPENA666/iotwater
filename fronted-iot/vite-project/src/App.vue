9369e11727acf5a21e73876171e008d413b98b96
Jonathan Peña <97184743+JJPENA666@users.noreply.github.com>
2024-07-16 00:32:59 -0500

    entorno proyecto y navbar

---
+++ fronted-iot/vite-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+ import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+  <h1 class="title">Bienvenido IOT WATER</h1>
+  <NavBar></NavBar>
+</template>
+
+<style>
+.title{
+  background-color: rgb(59, 59, 185);
+  color:white;
+}
+
+</style>
---
+++ fronted-iot/vite-project/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+    <ul>
+        <li>
+            <a href="#">Principal</a>
+        </li>
+        <li>
+            <a href="#">Medidor</a>
+        </li>
+        <li>
+            <a href="#">Ubicacion</a>
+        </li>
+        <li>
+            <a href="#">Gateway</a>
+        </li>
+        <li>
+            <a href="#">Consumo</a>
+        </li>
+        <li>
+            <a href="#">Usuarios</a>
+        </li>
+        
+    </ul>
+</template>
